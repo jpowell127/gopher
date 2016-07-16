cd8f98f34f55c1d677a50d05b6852e2c4f783b46
JPowell127 <jpowell.cr@gmail.com>
2016-07-16 15:44:46 -0500

    Basic List Functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
-import {About} from './about/about';
 import {Home} from './home/home';
 import {RepoBrowser} from './github/repo-browser/repo-browser';
 import {RepoList} from './github/repo-list/repo-list';
@@ -9,7 +8,6 @@ import {RepoDetail} from './github/repo-detail/repo-detail';
 const routes: RouterConfig = [
   { path: '', redirectTo: 'home', terminal: true },
   { path: 'home', component: Home },
-  { path: 'about', component: About },
   { path: 'github', component: RepoBrowser, children: [
     { path: ':org', component: RepoList, children: [
       { path: ':repo', component: RepoDetail },
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,32 @@
 import {Component} from '@angular/core';
+import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'home',
   pipes: [],
   providers: [],
-  directives: [],
-  styleUrls: ['./home.css'],
-  templateUrl: './home.html'
+  directives: [AlertComponent],
+  templateUrl: `
+    <form class="form-inline">
+      <input type="text" [(ngModel)]="item" placeholder="Gopher Item" (keyup.enter)="addItem(item)">
+      <button type="button" class='btn btn-primary' (click)="addItem(item)">Add Item</button>
+    </form>
+    <alert *ngFor="let item of items;let i = index" [type]="item.type" dismissible="true" (close)="deleteItem(i)">
+      {{ item?.msg }}
+    </alert>
+  `
 })
 export class Home {
+  public items: Array<Object> = [];
+  public item: String;
+
+  public deleteItem(i: number):void {
+    this.items.splice(i, 1);
+  }
+
+  public addItem(item: string):void {
+    this.items.push({msg: item, type: 'info', closable: true});
+    this.item = null;
+  }
+
 }
