3a01da58501acddd620b86a2d94137144669be89
JPowell127 <jpowell.cr@gmail.com>
2016-07-16 15:52:07 -0500

    removing deleted routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,10 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
 import {Home} from './home/home';
-import {RepoBrowser} from './github/repo-browser/repo-browser';
-import {RepoList} from './github/repo-list/repo-list';
-import {RepoDetail} from './github/repo-detail/repo-detail';
 
 const routes: RouterConfig = [
   { path: '', redirectTo: 'home', terminal: true },
-  { path: 'home', component: Home },
-  { path: 'github', component: RepoBrowser, children: [
-    { path: ':org', component: RepoList, children: [
-      { path: ':repo', component: RepoDetail },
-      { path: '', component: RepoDetail }
-    ]},
-    { path: '', component: RepoList}
-  ]}
+  { path: 'home', component: Home }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
